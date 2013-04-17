438e364b0972688a6589d6aab9b4bb058d7bc3b3
David Volm <david@volminator.com>
2013-04-17 11:53:02 -0500

    v0.0.1

--- auto-invoice.js
+++ auto-invoice.js
@@ -20,27 +20,64 @@
         root.returnExports = factory();
     }
 }(this, function() {
-    var argv = require('optimist')
-               .alias('t', 'template')
-               .alias('o', 'output')
-               .argv,
+    var opt = require('optimist')
+                .alias('t', 'template')
+                .describe('t', 'png template')
+                .default('t', 'invoice-template.png')
+                
+                .alias('o', 'output')
+                .describe('o', 'output mustache fn')
+                .default('o', '{{f}}-{{t}}.pdf')
+                
+                .alias('pc', 'priceCol')
+                .describe('pc', 'price column name')
+                .default('pc', 'Price')
+                
+                .alias('p', 'padding')
+                .describe('p', 'column padding in spaces')
+                .default('p', 7)
+                
+                .alias('d', 'decimal')
+                .describe('d', 'decimal places for cash')
+                .default('d', 2)
+                
+                .usage('auto-invoice [ option(s) ] [ csv filename(s) ]')
+                .demand(1),
+        argv = opt.argv,
         glob = require('glob'),
         async = require('async'),
+        S = require('string'),
+        Mustache = require('mustache'),
         PDFDocument = require('pdfkit'),
-        fs = require('fs');
+        Cash = require('cash.js'),
+        fs = require('fs'),
+        path = require('path');
     
-    var fn = argv._[0],
-        template = (typeof argv.t == 'undefined') ? 'invoice-template.png' : argv.t,
-        output = (typeof argv.o == 'undefined') ? 'invoice-{{f}}-{{d}}.pdf' : argv.o;
+    var fn = argv._,
+        template = argv.t,
+        output = argv.o,
+        priceCol = argv.pc,
+        padding = argv.p;
+    
+    Cash.automin(argv.d);
     
     async.parallel({
         "glob": function(cb) {
-            glob(fn, cb);
+            if(typeof fn == 'string') {
+                glob(fn, cb);
+            } else if(Array.isArray(fn)) {
+                cb(null, fn);
+            } else {
+                glob(fn.toString(), cb);
+            }
         },
         "exists": function(cb) {
             fs.exists(template, function(e) {
                 cb(null, e);
             });
+        },
+        "mustache": function(cb) {
+            cb(null, Mustache.compile(output));
         }
     }, function(err, res) {
         if(err) {
@@ -55,18 +92,106 @@
                     if(err) {
                         console.error(err);
                     } else {
-                        var doc = new PDFDocument();
-                        doc.image(template, 0, 0, {
-                            width: 612,
-                            height: 792
-                        });
-                        
+                        var docs = {};
                         async.each(res.glob, function(v, cb) {
-                            console.log(v + output);
-                            cb();
+                            docs[v] = new PDFDocument();
+                            
+                            var doc = docs[v],
+                                total = Cash.parse('0');
+                            
+                            doc.image(data, 0, 0, {
+                                width: 612,
+                                height: 792
+                            }).font(path.resolve(__dirname, 'UbuntuMono-R.ttf')).moveDown(7);
+                            
+                            fs.readFile(v, 'utf8', function(err, data) {
+                                var rows = S(data).lines(),
+                                    colSize = {},
+                                    _priceCol = -1;
+                                
+                                async.auto({
+                                    "findMax": function(cb) {
+                                        var row = -1;
+                                        
+                                        async.eachSeries(rows, function(v, cb) {
+                                            row++;
+                                            
+                                            var col = -1,
+                                                cols = v.split(',');
+                                            
+                                            async.eachSeries(cols, function(v, cb) {
+                                                col++;
+                                                var len = v.length;
+                                                
+                                                if(len > 0) {
+                                                    if(row === 0 && v === priceCol) {
+                                                        _priceCol = col;
+                                                    }
+                                                    
+                                                    if(row > 0 && col === _priceCol) {
+                                                        total = total.add(Cash.parse(v));
+                                                    }
+                                                    
+                                                    if(typeof colSize[col] != 'number') {
+                                                        colSize[col] = 0;
+                                                    }
+                                                    
+                                                    if(len > colSize[col]) {
+                                                        colSize[col] = len;
+                                                    }
+                                                }
+                                                
+                                                cb();
+                                            }, cb);
+                                        }, cb);
+                                    },
+                                    "doPDF": ["findMax", function(cb) {
+                                        var row = -1;
+                                        async.eachSeries(rows, function(v, cb) {
+                                            row++;
+                                            
+                                            var col = -1,
+                                                cols = v.split(','),
+                                                txt = '';
+                                            
+                                            async.eachSeries(cols, function(v, cb) {
+                                                col++;
+                                                
+                                                if(v.length > 0) {
+                                                    txt += S(v).padRight(colSize[col] + padding);
+                                                }
+                                                
+                                                cb();
+                                            }, function(err) {
+                                                if(txt.length > 1) {
+                                                    doc.text(txt);
+                                                }
+                                                
+                                                cb(err);
+                                            });
+                                        }, cb);
+                                    }],
+                                    "writeTotal": ["doPDF", function(cb) {
+                                        doc.moveDown(2).fontSize(25).text('Total: $' + total.toNumber(), {
+                                            align: 'right'
+                                        });
+                                        cb();
+                                    }]
+                                }, cb);
+                            });
                         }, function(err) {
-                            doc.write('./output.pdf');
-                            console.log(res);
+                            for(var _key in docs) {
+                                var key = _key,
+                                    doc = docs[key],
+                                    dash = S(key).dasherize().s;
+                                
+                                setTimeout(function() {
+                                    doc.write(res.mustache({
+                                        f: path.basename(dash, path.extname(dash)),
+                                        t: +new Date()
+                                    }));
+                                }, 0);
+                            }
                         });
                     }
                 });
