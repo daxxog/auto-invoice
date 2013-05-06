ecbdb9f15dfaee23cfe24193cc6b4cc62e110ac2
David Volm <david@volminator.com>
2013-05-06 17:08:49 -0500

    Added name/description

--- auto-invoice.js
+++ auto-invoice.js
@@ -1,5 +1,5 @@
-/* name
- * description
+/* auto-invoice
+ * Script that makes invoices.
  * (c) 2013 David (daXXog) Volm ><> + + + <><
  * Released under Apache License, Version 2.0:
  * http://www.apache.org/licenses/LICENSE-2.0.html  
